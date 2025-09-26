db07bcfe6e27f206ca1d2b72f05d13a33a737aa5
link10804k <62154757+link10804k@users.noreply.github.com>
2025-09-26 16:47:25 +0200

    Ejercicio3

--- Tema1/Ejercicio3/Ejercicio3.cpp
+++ Tema1/Ejercicio3/Ejercicio3.cpp
@@ -5,23 +5,54 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
-
+#include<vector>
 
 // función que resuelve el problema
-TipoSolucion resolver(TipoDatos datos) {
+bool resolver(int datos[], int pos, int nDatos) {
+    int max = datos[0];
+    for (int i = 1; i < pos + 1; ++i) {
+        if (datos[i] > max) {
+            max = datos[i];
+        }
+    }
 
+    bool menores = true;
+    int i = pos + 1;
 
+    while (i < nDatos && menores) {
+        if (datos[i] <= max) {
+            menores = false;
+        }
+        ++i;
+    }
+    return menores;
 }
 
 // Resuelve un caso de prueba, leyendo de la entrada la
 // configuración, y escribiendo la respuesta
 void resuelveCaso() {
     // leer los datos de la entrada
+    int nDatos;
+    int pos;
 
+    std::cin >> nDatos;
+    std::cin >> pos;
 
-    TipoSolucion sol = resolver(datos);
+    int* datos = new int[nDatos];
+    for (int i = 0; i < nDatos; ++i) {
+        std::cin >> datos[i];
+    }
+
+    bool sol = resolver(datos, pos, nDatos);
+    delete[] datos;
+
     // escribir sol
-
+    if (sol) {
+        std::cout << "SI" << std::endl;
+    }
+    else {
+        std::cout << "NO" << std::endl;
+    }
 
 }
 
